8f10f587b0c08f10df51e3c793119db94e69014a
Jeff Terrell <jeffaterrell@gmail.com>
2021-10-26 16:45:13 -0400

    update README and add more logic to test for removing numbers and spaces
    co-authored-by: Ella Tanttu <ellajtanttu@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,98 @@
 $(document).ready(function() {
-  const sample = "hello there ok green";
-  let test = [];
 
 
-  function wordCounter(apple) {
-    test = apple.split(' ')
-    let wordCount = 0;
-    test.forEach(function(element) {
-    wordCount += 1;
-    });
-    $("#result").text(wordCount);
-  }
-});  
 
-wordCounter(sample);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Sample code 1 from lesson *******
+
+//   const sample = "hello there ok green";
+//   let test = [];
+
+
+//   function wordCounter(apple) {
+//     test = apple.split(' ')
+//     let wordCount = 0;
+//     test.forEach(function(element) {
+//     wordCount += 1;
+//     });
+//     $("#result").text(wordCount);
+//   }
+// });
+
+// wordCounter(sample);
+
+
+
+// Sample code 2 from lesson *******
+
+// const text = "";
+
+// function wordCounter(text) {
+// 	const wordArray = text.split(' ');
+//   let wordCount = 0;
+//   wordArray.forEach(function(word) {
+//   	if (word.length > 0) {
+//     	wordCount ++;
+//     }	else {
+//     	return 0;
+//     }
+//   });
+//   $("#result").text(wordCount);
+// }
+
+// wordCounter(text);
+
+
+
+// Sample code 3 from lesson *******
+// const text = "";
+
+// function wordCounter(text) {
+// 	if (text.trim().length === 0) {
+//    	console.log(text.trim().length);
+//     return 0;
+//   }  
+//   let wordCount = 0;
+//   const wordArray = text.split(" ");
+//   wordArray.forEach(function(word) {
+//   	wordCount ++;
+//   });
+//   console.log(wordCount);
+//   /* $("#result").text(wordCount) */;
+// }
+
+// wordCounter(text);
+
+
+// Sample code 4 from lesson *******
+// const text = "hi there 77 19 in";
+
+// function wordCounter(text) {
+// 	if (text.trim().length === 0) {
+//    	console.log(text.trim().length);
+//     return 0;
+//   }  
+//   let wordCount = 0;
+//   const wordArray = text.split(" ");
+//   wordArray.forEach(function(element) { 
+//  		if (!Number(element)) {
+//   		wordCount ++;
+//     }
+//   });
+//   console.log(wordCount);
+  /* $("#result").text(wordCount) */
+// }
+
+// wordCounter(text);
